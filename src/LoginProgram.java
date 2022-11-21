ebeb3110567bf88f8825bd307aaa68c386b2b1d1
basitshah225 <basitshah225@gmail.com>
2022-11-21 16:56:39 +0000

    Initial commit

---
+++ src/LoginProgram.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class LoginProgram {
+
+    public static void main(String[] args) {
+        //initialise variables
+        String userName = "basitshah225";
+        String password = "123";
+        int maxNumOfAttempts = 5;
+        int attemptCounter = 0;
+        boolean validCredentials = false;
+
+        //create new scanner to read user input
+        Scanner input = new Scanner(System.in);
+        //loop until correct credentials or attempt limit reached
+        while (validCredentials == false && attemptCounter < maxNumOfAttempts ) {
+
+            attemptCounter++;
+            String inputUserName = ""; //reset user input to empty field after every attempt
+            String inputPassword = "";
+            //loop until user enters input for both fields per attempt
+            while (inputUserName.isEmpty()) {
+                System.out.print("Please enter your username: ");
+                inputUserName = input.nextLine();
+                if (inputUserName.isEmpty()) {
+                    System.out.println("Field can not be blank");
+                }
+            }
+            while (inputPassword.isEmpty()) {
+                System.out.print("Please enter your password: ");
+                inputPassword = input.nextLine();
+                if (inputPassword.isEmpty()) {
+                    System.out.println("Field can not be blank");
+                }
+            }
+            //escape loop if correct values entered
+            if (inputUserName.equals(userName) && inputPassword.equals(password)) {
+                validCredentials = true;
+                attemptCounter = 0;
+            }
+            else {
+                System.out.println("Incorrect User Name or Password! " + (maxNumOfAttempts - attemptCounter) + " attempt(s) remaining");
+            }
+        }
+        if (attemptCounter >= maxNumOfAttempts) {
+            System.out.println("Sorry maximum number of login attempts reached!");
+        }
+        else {
+            System.out.println("Congratulations, you have logged in!");
+        }
+    }
+}
